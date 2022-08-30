a35040b7ba834ac712c94ae3e6e3ae25326252ed
Meguiito <ignaciojaramillo54@gmail.com>
2022-08-30 01:09:10 -0400

    cambio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Users from "./componentes/Users";
 import  Principal  from "./componentes/Principal";
-import navbar from './componentes/navbar';
+import navbar from './componentes/Navbar';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Principal/>}/>
         <Route path="/login" element={<Users/>} />
-        <Route path="/navbar" element={<navbar/>}    />
+        <Route path="/navbar" element={<Navbar/>}    />
       </Routes>
   </Router>
 
--- frontend/src/componentes/navbar.js
+++ frontend/src/componentes/navbar.js
@@ -1,35 +1,27 @@
 import React from 'react';
-
-const navbar = () => {
+import { Link } from 'react-router-dom';
+const Navbar = () => {
   return (
     <div>
         <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <Link to="/Principal">
+        <img src='./kowloon.png' width="50"/>
+    </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+          <Link className="nav-link active" to="/Principal" >Inicio</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Usuarios</a>
+          <Link className="nav-link" to="/Users">Usuarios</Link>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#"></a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
+          </li>
       </ul>
     </div>
   </div>
@@ -39,4 +31,4 @@ const navbar = () => {
   )
 }
 
-export default navbar;
+export default Navbar;
