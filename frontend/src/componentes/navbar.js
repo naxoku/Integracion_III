7470e2acb4b048c041ad6e5a1e0f99bf147680f0
TOjedaOyarzo <tojedaoyarzo19@gmail.com>
2022-08-31 23:25:54 -0400

    Update navbar.js

--- frontend/src/componentes/navbar.js
+++ frontend/src/componentes/navbar.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Principal from './Principal';
-const Navbar = () => {
+function Navbar() {  
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-primary">
+        <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <Link to="/">
+    <Link to="/Principal">
         <img src='./kowloon.png' width="50"/>
     </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,10 +14,10 @@ const Navbar = () => {
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <Link className="nav-link active" to="/" >Inicio</Link>
+          <Link className="nav-link active" to="/Principal" >Inicio</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/login">Usuarios</Link>
+          <Link className="nav-link" to="/Users">Usuarios</Link>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#"></a>
