72be3e6d218bd4a72df7605d8bfd714ca8dc43ae
TOjedaOyarzo <tojedaoyarzo19@gmail.com>
2022-09-07 01:09:47 -0300

    a

--- frontend/src/componentes/navbar.js
+++ frontend/src/componentes/navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { checkIfIsLoggedIn, removeToken } from '../utils';
 
+
 export const Navbar = () => {
 
     const logeado = checkIfIsLoggedIn();
@@ -17,10 +18,11 @@ export const Navbar = () => {
 					{/* Logo del Navbar */}
 					<div>
 						<Link to="/">
-							<img className='me-3' src='./libroIcon.png' width="50"/>
+							<img className='me-2' src='./libroIcon.png' width="50" alt='PageLogo.png'/>
 						</Link>
 					</div>
-
+					<h4 className='mt-2 me-2'>Biblioteca Kowloon</h4>
+					
 					{/* Botón responsive design */}
   				  	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
   				  	  	<span class="navbar-toggler-icon"></span>
@@ -28,21 +30,24 @@ export const Navbar = () => {
 
 					{/* Lista de botones en el navbar */}
   				  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-  				  	  	<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-							<li class="nav-item me-3">
-                            	<a href="/" class="btn btn-primary" role="button" aria-current="page">
+  				  	  	<ul class="navbar-nav me-auto mb-lg-0">
+							<li class="d-flex m-1">
+                            	<a href="/" class="btn btn-primary container-md " role="button" aria-current="page">
                                 Página principal</a>
                         	</li>
-							<li class="nav-item me-3">
-                            	<a href="/" class="btn btn-primary" role="button" aria-current="page">
+							<li class="d-flex m-1">
+                            	<a href="/" class="btn btn-primary container-md" role="button" aria-current="page">
                                 Biblioteca</a>
                         	</li>
-							<li class="nav-item me-3">
-                            	<a href="/" class="btn btn-primary" role="button" aria-current="page">
+							<li class="d-flex m-1">
+                            	<a href="/" class="btn btn-primary container-md" role="button" aria-current="page">
                                 Repositorio</a>
                         	</li>
-
-                            {logeado && (
+							<li class="d-flex m-1">
+                            	<a href="/login" class="btn btn-primary container-md" role="button" aria-current="page">
+                                Iniciar sesión</a>
+                        	</li>
+							{logeado && (
 							  <li class="nav-item me-3">
                             	<a onClick= {cerrarSesion} href="/login" class="btn btn-primary" role="button" aria-current="page">
                                 Cerrar sesión</a>
@@ -55,11 +60,8 @@ export const Navbar = () => {
                                 Iniciar sesión</a>
                         	  </li>
                             )}
-
-
-
   				  	  	</ul>
-  				  	  	<form class="d-flex">
+  				  	  	<form class="d-flex m-1">
   				  	  	  	<input class="form-control me-2" type="search" placeholder="Ingrese una búsqueda..." aria-label="Search"/>
   				  	  	  	<button class="btn btn-primary" type="submit">Buscar</button>
   				  	  	</form>
@@ -70,4 +72,4 @@ export const Navbar = () => {
 	)
 }
 
-export default navbar;
+export default Navbar;
