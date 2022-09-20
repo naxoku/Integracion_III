d6657e44b75f43e1f12636adaa6475c46ea162a2
Meguiito <ignaciojaramillo54@gmail.com>
2022-09-19 23:50:31 -0400

    Create bibloteca.js

---
+++ frontend/src/componentes/bibloteca.js
@@ -0,0 +1,8 @@
+import React from "react"
+const bibloteca = () => {
+    return (
+        <div className='container-md border-danger'>
+            <h4 class="fw-bold mt-3">esta es la bibloteca</h4>
+        </div>
+    )
+}
