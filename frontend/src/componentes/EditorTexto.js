6f3e021060bfbe643a81c5f021dffa4af80ab6a4
SEBASTIÁN FRANCISCO IGNACIO CISTERNAS SANDOVAL <scisternas2020@alu.uct.cl>
2022-10-17 23:58:47 -0300

    Ajuste de los botones que vamos a usar
    para mas informacion revisar el plugin textButtons de froala

--- frontend/src/componentes/EditorTexto.js
+++ frontend/src/componentes/EditorTexto.js
@@ -13,37 +13,31 @@ class EditorTexto extends React.Component {
     render() {
         return (
             <div>
-                <FroalaEditor config={{
+                <FroalaEditor 
+                    config={{
+                        toolbarButtons: [
+                            'bold',
+                            'italic',
+                            'underline',
+                            'strikeThrough',
+                            'fontFamily',
+                            'fontSize',
+                            'alignLeft',
+                            'alignCenter',
+                            'alignRight',
+                            'alignJustify',
+                            'insertImage',
+                            'insertTable',
+                            'getPDF', 
+                            'print'
 
-'moreText': {
-
-  'buttons': ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting']
-
-},
-
-'moreParagraph': {
-
-  'buttons': ['alignLeft', 'alignCenter', 'formatOLSimple', 'alignRight', 'alignJustify', 'formatOL', 'formatUL', 'paragraphFormat', 'paragraphStyle', 'lineHeight', 'outdent', 'indent', 'quote']
-
-},
-
-'moreRich': {
-
-  'buttons': ['insertLink', 'insertImage', 'insertVideo', 'insertTable', 'emoticons', 'fontAwesome', 'specialCharacters', 'embedly', 'insertFile', 'insertHR']
-
-},
-
-'moreMisc': {
-
-  'buttons': ['undo', 'redo', 'fullscreen', 'print', 'getPDF', 'spellChecker', 'selectAll', 'html', 'help'],
-
-  'align': 'right',
-
-  'buttonsVisible': 2
-
-}
-
-}} />
+          
+          
+          
+          
+                        ]
+                    }}
+                />
             </div>
         );
     }
